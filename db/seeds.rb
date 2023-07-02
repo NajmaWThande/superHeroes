ceb34a6b2c70ceada04f4f4798d5c41aa222ee8d
Najma <najmathande@gmail.com>
2023-07-02 18:03:40 +0300

    done

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Hero.create(name: "Kamala Khan", super_name: "Ms. Marvel")
+Hero.create(name: "Doreen Green", super_name: "Squirrel Girl")
+Hero.create(name: "Gwen Stacy", super_name: "Spider-Gwen")
+
+Power.create(name: "super strength", description: "gives the wielder super-human strengths")
+Power.create(name: "flight", description: "gives the wielder the ability to fly through the skies at supersonic speed")
